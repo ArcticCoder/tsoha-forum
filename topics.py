45f2d151e06baa49caf70ab7ba4f8c5bfeaa26b2
Vili Sinervä <vili.sinerva@helsinki.fi>
2022-04-24 17:36:40 +0300

    Fix hiding of deleted topics for non-admins

--- topics.py
+++ topics.py
@@ -13,8 +13,8 @@ def get_all():
     else:
         sql = "SELECT A.id, A.topic, A.visible, COUNT(DISTINCT B.id) as thread_count,  "\
                 "COUNT(C.id) as message_count, TO_CHAR(MAX(C.time) AT TIME ZONE 'UTC', 'YYYY-MM-DD HH24:MI:SS') as latest FROM "\
-                "topics A LEFT JOIN threads B ON A.id=B.topic_id AND A.visible=true AND B.visible=true "\
-                "LEFT JOIN messages C ON B.id=C.thread_id "\
+                "topics A LEFT JOIN threads B ON A.id=B.topic_id AND B.visible=true "\
+                "LEFT JOIN messages C ON B.id=C.thread_id WHERE A.visible=true "\
                 "GROUP BY A.id ORDER BY A.visible DESC , A.topic;"
     return db.session.execute(sql).fetchall()
 
